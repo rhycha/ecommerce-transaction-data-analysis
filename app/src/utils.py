2e0f132c9c9527d1677a76baffe37690fa932973
Youngsub Lee <youngsle@c2r2s1.42luxembourg.lu>
2024-04-23 11:17:41 +0200

    add sampling for reducing data size

---
+++ app/src/utils.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+
+def sampling(filename,path=None):
+    # Adjust the fraction to the amount of data you want to sample, e.g., 0.1 for 10%
+    fraction = 0.1
+
+    # Load your data
+    # Replace 'your_data.csv' with the path to your file
+    df = pd.read_excel(filename)
+
+    # Sample the data
+    sampled_df = df.sample(frac=fraction, random_state=1)  # random_state for reproducibility
+
+    # Save the sampled data to a new file
+    sampled_df.to_csv(f'sampled_{filename}', index=False)
+
+
+if __name__=="__main__":
+    filename = f"Online Retail.xlsx"
+    sampling()
